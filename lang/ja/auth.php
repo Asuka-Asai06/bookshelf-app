2701157392f6a0b8b551cc76289ce193320d6412
Asuka Asai <asaiasuka06@gmail.com>
2026-07-23 11:23:36 +0900

    chore: バリデーションメッセージを日本語化

--- lang/ja/auth.php
+++ lang/ja/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '認証情報が登録内容と一致しません。',
+    'password' => 'パスワードが間違っています。',
+    'throttle' => 'ログイン試行回数が多すぎます。:seconds 秒後に再度お試しください。',
 
 ];
